08a4ea43842f8519b9b67dedca233a25f61e50eb
john-njuki <john.njuki.official@gmail.com>
2026-08-19 16:39:08 +0300

    FIX: fixed some breaking changes from prev commits regarding missing requirements breaking test runs

--- flake.nix
+++ flake.nix
@@ -13,14 +13,24 @@
       devShells.${system}.default = pkgs.mkShell {
         nativeBuildInputs = [
           pkgs.pyright
-        ];
-        buildInputs = [
           pkgs.python3
         ];
         shellHook = ''
           export LD_LIBRARY_PATH="${
             pkgs.lib.makeLibraryPath [
               pkgs.stdenv.cc.cc.lib
+              pkgs.zlib
+              pkgs.libGL
+              pkgs.glib
+              pkgs.xorg.libX11
+              pkgs.xorg.libXext
+              pkgs.xorg.libXrender
+              pkgs.xorg.libXinerama
+              pkgs.xorg.libXi
+              pkgs.xorg.libXrandr
+              pkgs.xorg.libXcursor
+              pkgs.xorg.libXfixes
+              pkgs.libxcb
             ]
           }:$LD_LIBRARY_PATH"
         '';
